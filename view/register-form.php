c23f06041c2def55222e64d901960ac1b5ebb65f
Chris Garcia <chrisg1245@hotmail.com>
2015-02-19 13:58:30 -0800

    finished video 39

--- view/register-form.php
+++ view/register-form.php
@@ -1,4 +1,10 @@
-<form>
+<?php
+    require_once(__DIR__ . "/../model/config.php");
+?>
+
+<h1>Register</h1>
+
+<form metho="post" action="<?php echo $path . "controller/create-user.php"; ?>">
     <div>
         <label for="email">Email: </label>   
         <input type="text" name="email" />
@@ -6,11 +12,16 @@
     
     <div>
         <label for="username">Username: </label>
-        <input type="password" name="password" />
+        <input type="text" name="username"/>
     </div>
     
     <div>
-        <button type="submit" name="submit"/>
+        <label for="password">Password</label>
+        <input type="password" name="password"/>
+    </div>
+    
+    <div>
+        <button type="submit">Submit</button>
     </div>
 </form>
 
